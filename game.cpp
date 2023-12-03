c47d6a5e0e7b0e3015c4135a944b6d1bdb2dfd4a
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2023-12-03 11:29:57 +0500

    Updated Code

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 void printBoard(char[][3]);
 void printing_input(char[][3], string, int);
@@ -6,18 +7,22 @@ bool checkForWinner(int, char[][3]);
 bool checkrows(char[][3], int);
 bool checkColumns(char[][3], int);
 bool checkdigonals(char[][3], int);
+bool ch3ckValidInput(string);
+bool checkForPrevInput(string, char[][3]);
 main()
 {
     char board[3][3] = {
         ' ', ' ', ' ',
         ' ', ' ', ' ',
         ' ', ' ', ' '};
-    printBoard(board);
     int count = 0;
+    string input;
     string Prev;
     while (count < 9)
     {
     start:
+        system("cls");
+        printBoard(board);
         if (count % 2 == 0)
         {
             cout << "Player X turn" << endl;
@@ -26,53 +31,60 @@ main()
         {
             cout << "Player O turn" << endl;
         }
-
-        string input;
         cout << "Enter (A1, B1, C1 e.g.): ";
         cin >> input;
-        if (Prev != input)
+        if (checkForPrevInput(input, board))
         {
-            Prev = input;
-            printing_input(board, input, count);
-            system("cls");
-            printBoard(board);
-            bool result = checkForWinner(count, board);
-            if (result)
+            if (ch3ckValidInput(input))
             {
-                if (count % 2 == 0)
+                printing_input(board, input, count);
+                system("cls");
+                printBoard(board);
+                bool result = checkForWinner(count, board);
+                if (result)
                 {
-                    cout << "Winner is X" << endl;
-                    break;
+                    if (count % 2 == 0)
+                    {
+                        cout << "Winner is X" << endl;
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Winner is O" << endl;
+                        break;
+                    }
                 }
-                else
-                {
-                    cout << "Winner is O" << endl;
-                    break;
-                }
+            }
+            else
+            {
+                cout << "Invalid Input!" << endl;
+                Sleep(1000);
+                system("cls");
+                goto start;
             }
         }
         else
         {
             cout << "Already Entered!" << endl;
+            Sleep(1000);
+            system("cls");
             goto start;
         }
         count++;
     }
     if (count == 9)
     {
-        cout << "The Match has been drawn!";
+        cout << "The Match has been drawns!";
     }
 }
 void printBoard(char arr[][3])
 {
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
+    cout << "   1   2   3" << endl;
+    cout << "A  " << arr[0][0] << " | " << arr[0][1] << " | " << arr[0][2] << endl;
+    cout << "  ---+---+---" << endl;
+    cout << "B  " << arr[1][0] << " | " << arr[1][1] << " | " << arr[1][2] << endl;
+    cout << "  ---+---+---" << endl;
+    cout << "C  " << arr[2][0] << " | " << arr[2][1] << " | " << arr[2][2] << endl;
 }
 void printing_input(char arr[][3], string input, int count)
 {
@@ -208,4 +220,32 @@ bool checkdigonals(char arr[][3], int count)
     }
 
     return false;
+}
+bool ch3ckValidInput(string input)
+{
+    if (input.length() == 2)
+    {
+        if (input[0] == 'A' || input[0] == 'B' || input[0] == 'C')
+        {
+            if (input[1] == '1' || input[1] == '2' || input[1] == '3')
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool checkForPrevInput(string input, char arr[][3])
+{
+    int character, num;
+    character = input[0] - 65;
+    num = input[1] - 49;
+    if (arr[character][num] == ' ')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
